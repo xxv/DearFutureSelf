fd9809e3368095dd3fdc4c614133d294d39c1469
Steve Pomeroy <steve@staticfree.info>
2012-08-17 16:45:32 -0400

    Fixes timeline entry with HW acceleration
    This also does the scaling outside the canvas, which improves the
    quality considerably.

--- src/info/staticfree/android/dearfutureself/TimelineEntry.java
+++ src/info/staticfree/android/dearfutureself/TimelineEntry.java
@@ -37,16 +37,18 @@ public class TimelineEntry extends View {
 
 	private float mScaleX;
 	private float mMultX;
-	private float mScaleCenterX;
 
 	private float mPrevX;
 	private float mPrevX1;
 
+	//@formatter:off
 	private static final int
 		STATE_STILL = 0,
 		STATE_TRANSLATING = 1,
 		STATE_SCALING = 2;
-	private int  mState = STATE_STILL;
+	//@formatter:on
+
+	private int mState = STATE_STILL;
 
 	private static final Paint RED_OUTLINE = new Paint();
 	static {
@@ -74,6 +76,7 @@ public class TimelineEntry extends View {
 		super(context);
 		init(context);
 	}
+
 	public TimelineEntry(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init(context);
@@ -84,12 +87,12 @@ public class TimelineEntry extends View {
 		init(context);
 	}
 
-	private void init(Context context){
+	private void init(Context context) {
 		mMarker = context.getResources().getDrawable(R.drawable.timeline_marker);
 
-		if (mStartTime == 0){
+		if (mStartTime == 0) {
 			mEndTime = System.currentTimeMillis();
-			//                 ms   →  s  → m  → h  → d
+			// ms → s → m → h → d
 			mStartTime = mEndTime - 1000 * 60 * 60 * 24;
 		}
 	}
@@ -103,18 +106,18 @@ public class TimelineEntry extends View {
 
 	@Override
 	protected void onRestoreInstanceState(Parcelable state) {
-		if ( !(state instanceof SavedState)){
+		if (!(state instanceof SavedState)) {
 			super.onRestoreInstanceState(state);
 			return;
 		}
 
-		final SavedState ss = (SavedState)state;
+		final SavedState ss = (SavedState) state;
 		super.onRestoreInstanceState(ss.getSuperState());
 		mStartTime = ss.mStartTime;
 		mEndTime = ss.mEndTime;
 	}
 
-	private static class SavedState extends BaseSavedState{
+	private static class SavedState extends BaseSavedState {
 		private final long mStartTime;
 		private final long mEndTime;
 
@@ -124,7 +127,7 @@ public class TimelineEntry extends View {
 			mEndTime = endTime;
 		}
 
-		public SavedState(Parcel in){
+		public SavedState(Parcel in) {
 			super(in);
 
 			mStartTime = in.readLong();
@@ -153,41 +156,36 @@ public class TimelineEntry extends View {
 		};
 	}
 
-	public void setOnChangeListener(OnChangeListener onChangeListener){
+	public void setOnChangeListener(OnChangeListener onChangeListener) {
 		mOnChangeListener = onChangeListener;
 	}
 
 	@Override
-	protected int getSuggestedMinimumHeight() {
-		return getBackground().getIntrinsicHeight();
-	}
-
-	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		int measuredWidth = View.MeasureSpec.getSize(widthMeasureSpec),
-			measuredHeight = View.MeasureSpec.getSize(widthMeasureSpec);
+		int measuredWidth = View.MeasureSpec.getSize(widthMeasureSpec), measuredHeight = View.MeasureSpec
+				.getSize(widthMeasureSpec);
 
-		switch (View.MeasureSpec.getMode(widthMeasureSpec)){
-		case View.MeasureSpec.UNSPECIFIED:
-			measuredWidth = getBackground().getIntrinsicWidth();
-			break;
-		case View.MeasureSpec.AT_MOST:
+		switch (View.MeasureSpec.getMode(widthMeasureSpec)) {
+			case View.MeasureSpec.UNSPECIFIED:
+				measuredWidth = getBackground().getIntrinsicWidth();
+				break;
+			case View.MeasureSpec.AT_MOST:
 
-			break;
-		case View.MeasureSpec.EXACTLY:
-			// already set!
-			break;
+				break;
+			case View.MeasureSpec.EXACTLY:
+				// already set!
+				break;
 		}
-		switch (View.MeasureSpec.getMode(heightMeasureSpec)){
-		case View.MeasureSpec.UNSPECIFIED:
-			measuredHeight = 100;
-			break;
-		case View.MeasureSpec.AT_MOST:
-			measuredHeight = 100;
-			break;
-		case View.MeasureSpec.EXACTLY:
-			// already set!
-			break;
+		switch (View.MeasureSpec.getMode(heightMeasureSpec)) {
+			case View.MeasureSpec.UNSPECIFIED:
+				measuredHeight = 100;
+				break;
+			case View.MeasureSpec.AT_MOST:
+				measuredHeight = 100;
+				break;
+			case View.MeasureSpec.EXACTLY:
+				// already set!
+				break;
 		}
 		setMeasuredDimension(measuredWidth, measuredHeight);
 	}
@@ -199,61 +197,57 @@ public class TimelineEntry extends View {
 		mPaddingRight = getPaddingRight();
 		mPaddingBottom = getPaddingBottom();
 		mPaddingTop = getPaddingTop();
-		mWidth = w - (mPaddingLeft);
-		mHeight = h - (mPaddingTop);
+		mWidth = w - (mPaddingLeft) - mPaddingRight;
+		mHeight = h - (mPaddingTop) - mPaddingBottom;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		final int w = mWidth;
 		final int h = mHeight;
-		final int hCenter = h/2;
+		final int hCenter = h / 2;
 
 		final long timelineW = mEndTime - mStartTime;
-		mScaleX = getWidth() / (float)timelineW;
-		mMultX = (float)timelineW / getWidth();
+		mScaleX = getWidth() / (float) timelineW;
+		mMultX = (float) timelineW / getWidth();
 
 		canvas.save();
-		//canvas.clipRect(mPaddingLeft, mPaddingTop, w, h);
+		canvas.translate(mPaddingLeft, mPaddingTop);
+
 		// main axis
-		canvas.drawLine(mPaddingLeft, h/2 + mPaddingTop, w, h/2 + mPaddingTop, PAINT_AXIS);
-		//canvas.drawRect(mPaddingLeft, mPaddingTop, w, h, RED_OUTLINE);
-		//final float scaleX = w/(float)timelineW;
-		canvas.scale(mScaleX, 1, mScaleCenterX, 0);
-		canvas.translate(-mStartTime, h/2);
+		canvas.drawLine(0, hCenter, w, hCenter, PAINT_AXIS);
 
 		// show the disabled region
-		if (mMinTime > mStartTime){
-			canvas.drawRect(mPaddingLeft, mPaddingTop - h/2, mMinTime, h/2, PAINT_DISABLED);
+		if (mMinTime > mStartTime) {
+			canvas.drawRect(0, 0, (mMinTime - mStartTime) * mScaleX, h, PAINT_DISABLED);
 		}
 
-//		// draw minor ticks
-//		for (long xMarker = mStartTime - (mStartTime % X_SCALE); xMarker < mEndTime; xMarker += X_SCALE){
-//			canvas.drawLine(xMarker, -MAJOR_TICK_SIZE, xMarker, MAJOR_TICK_SIZE, PAINT_MAJOR_TICKS);
-//		}
 		// draw minor ticks
-		for (long xMarker = mStartTime - (mStartTime % X_SCALE_SMALL); xMarker < mEndTime; xMarker += X_SCALE_SMALL){
-			if (xMarker % X_SCALE == 0){
-				canvas.drawLine(xMarker, -MAJOR_TICK_SIZE, xMarker, MAJOR_TICK_SIZE, PAINT_MAJOR_TICKS);
-			}else{
-				canvas.drawLine(xMarker, -MINOR_TICK_SIZE, xMarker, MINOR_TICK_SIZE, PAINT_MINOR_TICKS);
+		for (long xMarker = mStartTime - (mStartTime % X_SCALE_SMALL); xMarker < mEndTime; xMarker += X_SCALE_SMALL) {
+			if (xMarker % X_SCALE == 0) {
+				canvas.drawLine((xMarker - mStartTime) * mScaleX, hCenter + MAJOR_TICK_SIZE,
+						(xMarker - mStartTime) * mScaleX, hCenter - MAJOR_TICK_SIZE,
+						PAINT_MAJOR_TICKS);
+			} else {
+				canvas.drawLine((xMarker - mStartTime) * mScaleX, hCenter, (xMarker - mStartTime)
+						* mScaleX, hCenter - MINOR_TICK_SIZE, PAINT_MINOR_TICKS);
 			}
 		}
 		canvas.restore();
 
-		mMarker.setBounds(0,0,w, h);
+		mMarker.setBounds(0, 0, w, h);
 		mMarker.draw(canvas);
 
 	}
 
-	public long getTime(){
+	public long getTime() {
 		return (mEndTime - mStartTime) / 2 + mStartTime;
 	}
 
-	private void enforceMinimum(){
+	private void enforceMinimum() {
 		final long curTime = getTime();
 		// ensure that the start time is never below the min time
-		if (curTime < mMinTime){
+		if (curTime < mMinTime) {
 			final long minOffset = mMinTime - curTime;
 			mStartTime += minOffset;
 			mEndTime += minOffset;
@@ -263,21 +257,22 @@ public class TimelineEntry extends View {
 	/**
 	 * Sets the current time.
 	 *
-	 * @param time the time in milliseconds
+	 * @param time
+	 *            the time in milliseconds
 	 */
-	public void setTime(long time){
+	public void setTime(long time) {
 		final long curtime = getTime();
 		final long offset = time - curtime;
 		mEndTime += offset;
 		mStartTime += offset;
 
-		//enforceMinimum();
+		// enforceMinimum();
 
 		invalidate();
 		notifyListener();
 	}
 
-	public void setMinimumTime(long minTime){
+	public void setMinimumTime(long minTime) {
 		mMinTime = minTime;
 		invalidate();
 	}
@@ -285,14 +280,15 @@ public class TimelineEntry extends View {
 	/**
 	 * Sets the range. This is essentially the zoom scale.
 	 *
-	 * @param range the size of the range entry, in milliseconds
+	 * @param range
+	 *            the size of the range entry, in milliseconds
 	 */
-	public void setRange(long range){
+	public void setRange(long range) {
 		final long offset = range - (mEndTime - mStartTime) / 2;
 		mEndTime += offset;
 		mStartTime -= offset;
 
-		//enforceMinimum();
+		// enforceMinimum();
 
 		invalidate();
 		notifyListener();
@@ -301,10 +297,11 @@ public class TimelineEntry extends View {
 	/**
 	 * Translates the timeline by a certain number of pixels.
 	 *
-	 * @param pixels the number of pixels to offset by
+	 * @param pixels
+	 *            the number of pixels to offset by
 	 */
-	public void translateTimeline(float pixels){
-		final long offset = (long)(mMultX * pixels);
+	public void translateTimeline(float pixels) {
+		final long offset = (long) (mMultX * pixels);
 
 		mStartTime += offset;
 		mEndTime += offset;
@@ -318,11 +315,13 @@ public class TimelineEntry extends View {
 	/**
 	 * Scales the timeline by a certain number of pixels, centered at the given center.
 	 *
-	 * @param center not currently used
-	 * @param pixels the number of pixels to scale it by
+	 * @param center
+	 *            not currently used
+	 * @param pixels
+	 *            the number of pixels to scale it by
 	 */
-	public void scaleTimeline(float center, float pixels){
-		final long offset = (long)(mMultX * pixels) / 2;
+	public void scaleTimeline(float center, float pixels) {
+		final long offset = (long) (mMultX * pixels) / 2;
 		mEndTime += offset;
 		mStartTime -= offset;
 
@@ -332,85 +331,88 @@ public class TimelineEntry extends View {
 		notifyListener();
 	}
 
-	private void notifyListener(){
-		if (mOnChangeListener != null){
-			mOnChangeListener.onChange(mStartTime + (mEndTime - mStartTime)/2, mStartTime, mEndTime);
+	private void notifyListener() {
+		if (mOnChangeListener != null) {
+			mOnChangeListener.onChange(mStartTime + (mEndTime - mStartTime) / 2, mStartTime,
+					mEndTime);
 		}
 	}
 
-    /**
-     * Tries to claim the user's drag motion, and requests disallowing any
-     * ancestors from stealing events in the drag.
-     */
-    private void attemptClaimDrag() {
-    	final ViewParent parent = getParent();
-        if (parent != null) {
-            parent.requestDisallowInterceptTouchEvent(true);
-        }
-    }
+	/**
+	 * Tries to claim the user's drag motion, and requests disallowing any ancestors from stealing
+	 * events in the drag.
+	 */
+	private void attemptClaimDrag() {
+		final ViewParent parent = getParent();
+		if (parent != null) {
+			parent.requestDisallowInterceptTouchEvent(true);
+		}
+	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		final int action = event.getAction();
+
 		final int pointerID = (action & MotionEvent.ACTION_POINTER_ID_MASK) >> MotionEvent.ACTION_POINTER_ID_SHIFT;
 
-		switch (action & MotionEvent.ACTION_MASK){
-		case MotionEvent.ACTION_DOWN:{
-			attemptClaimDrag();
-			mState = STATE_TRANSLATING;
-			mPrevX = event.getX(0);
-			//mScaleCenterX = mPrevX;
-
-			return true;
-		}
-
-		case MotionEvent.ACTION_POINTER_DOWN:{
-			attemptClaimDrag();
-
-			if (pointerID == 1){
-				mPrevX1 = event.getX(1);
-				mState = STATE_SCALING;
-			}
-			return true;
-		}
-
-		case MotionEvent.ACTION_MOVE:{
-
-
-			switch (mState){
-			case STATE_TRANSLATING:
-				translateTimeline(mPrevX - event.getX(0));
-				break;
-
-			case STATE_SCALING:
-				final float x1 = event.getX(1), x0 = event.getX(0);
-				if (x1 > x0){
-					scaleTimeline(0, (mPrevX1- x1) - (mPrevX - x0) );
-				}else{
-					scaleTimeline(0, (mPrevX- x0) - (mPrevX1 - x1) );
-				}
-				mPrevX1 = event.getX(1);
-				break;
-			}
-			mPrevX = event.getX(0);
-			return true;
-		}
-		case MotionEvent.ACTION_POINTER_UP:
-		case MotionEvent.ACTION_UP:{
-
-			switch (mState){
-			case STATE_SCALING:
+		switch (action & MotionEvent.ACTION_MASK) {
+			case MotionEvent.ACTION_DOWN: {
+				attemptClaimDrag();
 				mState = STATE_TRANSLATING;
-				break;
+				mPrevX = event.getX(0);
+				// mScaleCenterX = mPrevX;
 
-			case STATE_TRANSLATING:
-				mState = STATE_STILL;
+				return true;
 			}
 
-		}return true;
+			case MotionEvent.ACTION_POINTER_DOWN: {
+				attemptClaimDrag();
 
-		default:
-			return super.onTouchEvent(event);
+				if (pointerID == 1) {
+					mPrevX1 = event.getX(1);
+					mState = STATE_SCALING;
+				}
+				return true;
+			}
+
+			case MotionEvent.ACTION_MOVE: {
+
+				switch (mState) {
+					case STATE_TRANSLATING:
+						translateTimeline(mPrevX - event.getX(0));
+						break;
+
+					case STATE_SCALING:
+						final float x1 = event.getX(1),
+						x0 = event.getX(0);
+						if (x1 > x0) {
+							scaleTimeline(0, (mPrevX1 - x1) - (mPrevX - x0));
+						} else {
+							scaleTimeline(0, (mPrevX - x0) - (mPrevX1 - x1));
+						}
+						mPrevX1 = event.getX(1);
+						break;
+				}
+				mPrevX = event.getX(0);
+				return true;
+			}
+			case MotionEvent.ACTION_POINTER_UP:
+			case MotionEvent.ACTION_UP: {
+
+				switch (mState) {
+					case STATE_SCALING:
+						mState = STATE_TRANSLATING;
+						break;
+
+					case STATE_TRANSLATING:
+						mState = STATE_STILL;
+				}
+
+			}
+				return true;
+
+			default:
+				return super.onTouchEvent(event);
 		}
 
 	}
